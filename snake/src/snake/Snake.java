9ebaaf3be3c946d85e01c503a4ef5c901641de10
JohnnyZhang19 <68079378+JohnnyZhang19@users.noreply.github.com>
2020-07-30 13:05:24 -0600

    Update Snake.java

--- snake/src/snake/Snake.java
+++ snake/src/snake/Snake.java
@@ -30,14 +30,59 @@ public class Snake {
 	
 	public void addFromHead(Node node) {
 		body.addFirst(node);
+		head = body.getFirst();
+		
 	}
 	
 	public void addFromTail(Node node) {
 		body.addLast(node);
+		tail = body.getLast();
 	}
 	
-	public void deleteFromTail(Node node) {
+	public void deleteFromTail() {
 		body.removeLast();
+		tail = body.getLast();
+	}
+	
+	public void move(int direction) {
+		// TODO Auto-generated method stub
+
+	}
+	
+	public void right() {
+		addFromHead(new Node(getHead().getX()+1, getHead().getY(),5));
+		deleteFromTail();
+	}
+
+	public void up() {
+		addFromHead(new Node(getHead().getX(),getHead().getY()-1,5));
+		deleteFromTail();
+	}
+
+	public void down() {
+		addFromHead(new Node(getHead().getX(), getHead().getY()+1,5));
+		deleteFromTail();
+	}
+
+	public void left(){
+		addFromHead(new Node(getHead().getX()-1, getHead().getY(),5));
+		deleteFromTail();
+	}
+	
+	/**
+	 * 
+	 * @param direction(1: up, 2: right, 3: down, 4:left) 
+	 */
+	public void eat(int direction) {
+		if (direction==1) {
+			addFromHead(new Node(getHead().getX(),getHead().getY()-1,5));
+		}else if (direction==2) {
+			addFromHead(new Node(getHead().getX()+1, getHead().getY(),5));
+		}else if (direction==3) {
+			addFromHead(new Node(getHead().getX(), getHead().getY()+1,5));
+		}else {
+			addFromHead(new Node(getHead().getX()-1, getHead().getY(),5));
+		}
 	}
 
 	
